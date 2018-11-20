0f9225d4db59d7a1954b31cfb20737d0f4d9d781
Emanuele <emanuele.lamalfa@mail.polimi.it>
2018-11-20 19:06:59 +0100

    1d-convolution: test derivative for one layer

--- cnn/series_to_matrix.py
+++ cnn/series_to_matrix.py
@@ -7,8 +7,9 @@ Created on Fri Nov 16 18:05:40 2018
 Transform a series into a matrix, based on striding and kernel's shape value.
 This way SIMD operations can be used with convolution: in particular from 
  convolution one may use conv_1d with simd flag set to True.
-Please note that this operation is expansive and should be used when convolution
- is done with fixed-size (kernels, striding).
+This function is employed, among the others, in the backpropagation phase to 
+ calculate the contribution of an activation function to the variables that 
+ compose it.
 """
 
 import numpy as np
@@ -18,17 +19,14 @@ Takes as input:
     series:array, a (1,n) array;
     k_shape:int, specifies the kernel shape;
     stride:int, specifies striding value, i.e. how many values are skipped at a
-               new iteration;
-    ignore_last:boolean, specifies whether to ignore or not the last elements if 
-                        their dimension is lower than k_shape.
+               new iteration.
 Returns:
     res:matrix, a (i, k_shape) matrix that can be used to cast convolution as SIMD multiply
                operation: i can be calculated as $\ceil{\dfrac{len(series)-k_shape+1}{striding}}$
 """
 def series_to_matrix(series, 
                      k_shape, 
-                     striding=1,
-                     ignore_last=True):
+                     striding=1):
     
     res = np.zeros(shape=(int((series.shape[1]-k_shape)/striding)+1,
                    k_shape)
---
+++ test/test_derivative_convolution_1d_1_layer.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 20 18:51:32 2018
+
+@author: Emanuele
+
+Test backpropagation for one layer of 1d-convolution
+"""
+
+import sys
+sys.path.append('../cnn')
+import convolution as conv
+import series_to_matrix as stm
+
+import numpy as np
+
+# exponential activation function
+def exp(series, weights, stride):
+    
+    res = conv.conv_1d(series, weights, stride)
+    return np.exp(res)
+
+if __name__ == '__main__':
+    
+    inp = np.random.rand(1, 100)
+    
+    weights = np.random.rand(1, 10)
+    stride = 1
+    
+    # derivative by implementation
+    derivative_by_implementation = exp(inp, weights, stride)@stm.series_to_matrix(inp, weights.shape[1], stride)
+    
+    # derivative by definition
+    derivative_by_def = list()
+    epsilon = 1e-5
+    for i in range(weights.shape[1]):
+        
+        weights[:,i] += epsilon
+        f_plus = exp(inp, weights, stride)
+        weights[:,i] -= 2*epsilon
+        f_minus = exp(inp, weights, stride)
+        weights[:,i] += epsilon
+        
+        derivative_by_def.append(np.sum((f_plus-f_minus)/(2*epsilon)))
+        
+    error = np.linalg.norm(derivative_by_def-derivative_by_implementation)
+    print("Error in estimating derivative of one layer convolution with implementation is ", error)
+
+    
+    
+    
+    
