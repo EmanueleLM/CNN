1c9276756d841e1ee3f0136ccbf8c61831f3ea87
Emanuele <emanuele.lamalfa@mail.polimi.it>
2018-12-04 10:16:01 +0100

    test net activation

--- test/test_net_activation.py
+++ test/test_net_activation.py
@@ -13,6 +13,7 @@ import sys as sys
 
 sys.path.append('../cnn')
 
+import activations as act
 import convolution as conv
 import layer_convolutional as layer_conv
 import layer_dense as layer_dense
@@ -23,16 +24,19 @@ if __name__ == '__main__':
     
     net_blocks = {'n_inputs': 1000, 
                   'layers': [
-                          {'type': 'conv', 'activation': 'relu', 'shape': (1, 5), 'stride': 5}, 
+                          {'type': 'conv', 'activation': 'exp', 'shape': (1, 5), 'stride': 5}, 
                           {'type': 'conv', 'activation': 'relu', 'shape': (1, 4), 'stride': 2},
-                          {'type': 'conv', 'activation': 'relu', 'shape': (1, 7), 'stride': 3},
+                          {'type': 'conv', 'activation': 'exp', 'shape': (1, 7), 'stride': 3},
                           {'type': 'dense', 'activation': 'linear', 'shape': (None, 150)},
-                          {'type': 'dense', 'activation': 'linear', 'shape': (None, 20)}
+                          {'type': 'dense', 'activation': 'relu', 'shape': (None, 20)}
                           ]
                   }
     
     # create the net
     net = nn.NN(net_blocks)
+    
+    # initialize the parameters to random values between [-1, 1]
+    net.init_parameters(['uniform', -1., 1.])
     
     input_ = np.random.rand(1, net.n_inputs)
     
@@ -44,14 +48,14 @@ if __name__ == '__main__':
             weights = net.layers[i].weights
             stride = net.layers[i].stride
             tmp = conv.conv_1d(tmp, weights, stride)
-            print(i, tmp.shape)
+            tmp = act.dict_activations[net.layers[i].act](tmp)
         
         elif isinstance(net.layers[i], layer_dense.Dense): 
             
             weights = net.layers[i].weights
             bias = net.layers[i].bias
             tmp = np.dot(tmp, weights) + bias
-            print(i, tmp.shape)
+            tmp = act.dict_activations[net.layers[i].act](tmp)
             
         output_by_definition = tmp
 
