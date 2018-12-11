solved bug with backprop for layers after the closest to the dense ones

cnn/nn.py
@@ -362,7 +362,7 @@ class NN(object):
                     #  themselves.
                     else:
                         
-                        tmp = stm.series_to_tensor(self.layers[l].input_, 
+                        tmp = stm.series_to_tensor(img_tmp, 
                                                    self.layers[j].weights.shape, 
                                                    self.layers[j].stride)      
                         tmp = np.multiply(tmp, self.derivatives[l])
@@ -412,7 +412,7 @@ def NN_Compressed(object):
   abilitate this snippet if you are sure what you are doing.
 """
 
-verbose = True
+verbose = False
 
 if verbose is True:
     

test/experiments/prediction_topix.py
@@ -21,13 +21,13 @@ import utils_topix as utils
 
 if __name__ == '__main__':
     
-    net_blocks = {'n_inputs': 5, 
+    net_blocks = {'n_inputs': 25, 
                   'layers': [
-                          {'type': 'conv', 'activation': 'relu', 'shape': (1, 2), 'stride': 1}, 
-                          {'type': 'conv', 'activation': 'relu', 'shape': (1, 2), 'stride': 1},
-                          {'type': 'conv', 'activation': 'relu', 'shape': (1, 2), 'stride': 1},
-                          {'type': 'dense', 'activation': 'relu', 'shape': (None, 35)},                    
-                          {'type': 'dense', 'activation': 'relu', 'shape': (None, 1)}
+                          {'type': 'conv', 'activation': 'relu', 'shape': (25, 2), 'stride': 3}, 
+                          {'type': 'conv', 'activation': 'relu', 'shape': (25, 2), 'stride': 3}, 
+                          {'type': 'conv', 'activation': 'relu', 'shape': (25, 2), 'stride': 3}, 
+                          {'type': 'dense', 'activation': 'tanh', 'shape': (None, 35)},                    
+                          {'type': 'dense', 'activation': 'tanh', 'shape': (None, 1)}
                           ]
                   }
     
@@ -35,7 +35,7 @@ if __name__ == '__main__':
     net = nn.NN(net_blocks)
     
     # initialize the parameters
-    net.init_parameters(['uniform', -.1, .1])
+    net.init_parameters(['uniform', -1e-4, 1e-4])
 
     # create the batches from topix dataset
     X_train, Y_train, X_valid, Y_valid, X_test, Y_test = utils.generate_batches(
@@ -52,7 +52,7 @@ if __name__ == '__main__':
     Y_test = (Y_test-np.min(Y_test))/(np.max(Y_test)-np.min(Y_test))   
     Y_valid = (Y_valid-np.min(Y_valid))/(np.max(Y_valid)-np.min(Y_valid))    
     
-    epochs_train = 25
+    epochs_train = 10
        
     # train
     for e in range(epochs_train):
@@ -73,7 +73,7 @@ if __name__ == '__main__':
             net.backpropagation(target=target, 
                                 loss='L2', 
                                 optimizer='sgd', 
-                                l_rate=5e-3,
+                                l_rate=1e-4,
                                 update=True)
                         
     # validation: calculate error and estimate its mean and variance
@@ -89,13 +89,13 @@ if __name__ == '__main__':
         
         net.activation(input_, accumulate=True)
         
-        # backrpop after prediction
-        net.derivative(None)                    
-        net.backpropagation(target=target, 
-                            loss='L2', 
-                            optimizer='sgd', 
-                            l_rate=5e-3,
-                            update=True)
+#        # backrpop after prediction
+#        net.derivative(None)                    
+#        net.backpropagation(target=target, 
+#                            loss='L2', 
+#                            optimizer='sgd', 
+#                            l_rate=5e-3,
+#                            update=True)
         
         errors_valid[i] = net.output - target
         
@@ -118,13 +118,13 @@ if __name__ == '__main__':
         net.activation(input_, accumulate=True)
         prediction = net.output
         
-        # backrpop after prediction
-        net.derivative(None)                    
-        net.backpropagation(target=target, 
-                            loss='L2', 
-                            optimizer='sgd', 
-                            l_rate=5e-2,
-                            update=True)
+#        # backrpop after prediction
+#        net.derivative(None)                    
+#        net.backpropagation(target=target, 
+#                            loss='L2', 
+#                            optimizer='sgd', 
+#                            l_rate=5e-2,
+#                            update=True)
         
         predictions[i] = prediction
         errors_test[i] = utils.gaussian_pdf(errors_test[i], 

test/test_backpropagation_n_layers.py
@@ -44,9 +44,10 @@ if __name__ == '__main__':
     
     net_blocks = {'n_inputs': 24, 
                   'layers': [
-                          {'type': 'conv', 'activation': 'relu', 'shape': (1, 3), 'stride': 2}, 
-                          {'type': 'conv', 'activation': 'relu', 'shape': (1, 4), 'stride': 1},
-                          {'type': 'conv', 'activation': 'relu', 'shape': (1, 3), 'stride': 1},
+                          {'type': 'conv', 'activation': 'relu', 'shape': (2, 5), 'stride': 1}, 
+                          {'type': 'conv', 'activation': 'relu', 'shape': (10, 3), 'stride': 1}, 
+                          {'type': 'conv', 'activation': 'relu', 'shape': (10, 3), 'stride': 1}, 
+                          {'type': 'conv', 'activation': 'relu', 'shape': (10, 3), 'stride': 1}, 
                           {'type': 'dense', 'activation': 'relu', 'shape': (None, 35)},                    
                           {'type': 'dense', 'activation': 'relu', 'shape': (None, 1)}
                           ]
@@ -175,31 +176,38 @@ if __name__ == '__main__':
         # convolutional layer
         elif net.layers[n].type == 'conv':
                                     
-            # bias                 
-            for i in range(net.layers[n].weights.shape[1]):
+            # weights                 
+            for i in range(net.layers[n].weights.shape[0]):
+                
+                for j in range(net.layers[n].weights.shape[1]):
             
-                weights_conv[n_conv][:,i] += 2*epsilon
-                
-                f_plus = np.sum(activation(input_,
-                                    weights_conv, stride_conv, act_conv, 
-                                    weights_dense, bias_dense, act_dense)
-                                )
-                
-                weights_conv[n_conv][:,i] -= 2*epsilon
-        
-                f_minus = np.sum(activation(input_,
-                                    weights_conv, stride_conv, act_conv, 
-                                    weights_dense, bias_dense, act_dense)
-                                )
-                
-                weights_conv[n_conv][:,i] += epsilon
-                                    
-                derivative_by_def.append((f_plus-f_minus)/(2*epsilon))
+                    weights_conv[n_conv][i,j] += 2*epsilon
+                    
+                    f_plus = np.sum(activation(input_,
+                                        weights_conv, stride_conv, act_conv, 
+                                        weights_dense, bias_dense, act_dense)
+                                    )
+                    
+                    weights_conv[n_conv][i,j] -= 2*epsilon
             
+                    f_minus = np.sum(activation(input_,
+                                        weights_conv, stride_conv, act_conv, 
+                                        weights_dense, bias_dense, act_dense)
+                                    )
+                    
+                    weights_conv[n_conv][i,j] += epsilon
+                                        
+                    derivative_by_def.append((f_plus-f_minus)/(2*epsilon))
+                
             n_conv += 1
 
     # evaluate error
-    derivative_by_def = np.array(derivative_by_def).flatten()
+    derivative_by_def = np.array(derivative_by_def).flatten()    
+    
+    # exclude first weights of convolution
+    conv_weights = np.sum(np.prod(l.weights.shape) for l in net.layers if l.type=='conv')
+    derivative_by_calc = derivative_by_calc[:conv_weights]
+    derivative_by_def = derivative_by_def[:conv_weights]
     
     error = np.linalg.norm((derivative_by_calc-derivative_by_def)/np.linalg.norm(derivative_by_calc))
     