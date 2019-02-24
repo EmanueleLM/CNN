e61cb1df092b6e066f1275b96e015983a6f43efa
Emanuele <emanuele.lamalfa@mail.polimi.it>
2019-02-24 13:35:45 +0100

    Tutorial

--- README.md
+++ README.md
@@ -1,17 +1,41 @@
 # Convolutional Neural Networks for Time Series Analysis
 ## Package to instantiate 1-D CNNs and train them on signals datasets
 
-## Things implemented so far, 07/12/2018: ##
+## Things implemented so far: ##
 
 - customizable deep net (specify with one line of code the net, from the topology to the activations);
-  - all from backpropagation to check gradient routine depend only on numpy;
+  - all from backpropagation to check gradient routine depends only on numpy;
   - easy to extend activations, derivatives, parameters initialization etc.
   
-- kernels can be matrices (bidimensional);
-  
-- test on TOPIX dataset: anomaly detection with gaussian index variation.
 
 TODO:
 
+- refactor code and package's name to handle 2D/3D images;
+
 - topologies that mix layers of convolutionals and dense type: so far it is possible to specify
   n>0 convolutional layers followed by m>0 dense layers.
+
+## Create a double-layer CNN followed by a final double-dense layer
+
+```python
+import nn as nn  # nn.py from this package
+import numpy as np
+
+# define networks layers
+net_blocks = {'n_inputs': 50, 
+              'layers': [
+                      {'type': 'conv', 'activation': 'leaky_relu', 'shape': (15, 2), 'stride': 3},                      
+                      {'type': 'conv', 'activation': 'leaky_relu', 'shape': (30, 2), 'stride': 3},                      
+                      {'type': 'dense', 'activation': 'relu', 'shape': (None, 75)},                    
+                      {'type': 'dense', 'activation': 'relu', 'shape': (None, 1)}
+                      ]
+              }
+    
+# create the net    
+net = nn.NN(net_blocks)
+    
+# initialize the parameters
+net.init_parameters(['uniform', -.1e-1, 1e-1])
+```
+
+Now you can check one of the test in the /test/experiments forlder to see how to run this model against a dataset.
---
+++ test/experiments/prediction_naive_dataset_test_gaussian.py
@@ -0,0 +1,141 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 13 15:58:03 2019
+
+@author: Emanuele
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+import scipy.stats as scistats
+import sys
+
+sys.path.append('../../cnn')
+sys.path.append('../experiments/data_utils')
+
+import nn as nn
+import utils_topix as utils
+
+
+if __name__ == '__main__':
+    
+    net_blocks = {'n_inputs': 1, 
+                  'layers': [ 
+                          {'type': 'dense', 'activation': 'linear', 'shape': (None, 5)},                    
+                          {'type': 'dense', 'activation': 'linear', 'shape': (None, 1)}
+                          ]
+                  }
+    
+    # create the net    
+    net = nn.NN(net_blocks)
+    
+    # initialize the parameters
+    net.init_parameters(['uniform', -.1e-1, 1e-0])
+
+    # create the batches from topix dataset
+    X_train, Y_train, X_valid, Y_valid, X_test, Y_test = utils.generate_batches(
+                                                              filename='data/naive_dataset.csv', 
+                                                              window=net.n_inputs, mode='validation', 
+                                                              non_train_percentage=.3,
+                                                              val_rel_percentage=.5,
+                                                              normalize=True,
+                                                              time_difference=False)       
+
+    epochs_train = 10
+       
+    # train
+    for e in range(epochs_train):
+        
+        print("Training epoch ", e+1)
+        
+        indices = np.arange(Y_train.shape[0])
+        np.random.shuffle(indices)
+        X_train = X_train[indices]
+        Y_train = Y_train[indices]
+                
+        for (input_, target) in zip(X_train, Y_train):
+                                                
+            # format input and prediction
+            input_ = input_[np.newaxis,:]
+            target = np.array([target])[np.newaxis,:]
+                      
+            # activate and caluclate derivative for each layer, given the input
+            net.activation(input_, accumulate=True)
+            net.derivative(None)
+                        
+            # execute the backpropagation with the input that has been memorized previously
+            net.backpropagation(target=target, 
+                                loss='L2', 
+                                optimizer='sgd', 
+                                l_rate=1e-1,
+                                update=True)
+                        
+    # validation: calculate error and estimate its mean and variance
+    errors_valid = np.zeros(shape=len(X_valid))
+    i = 0
+    
+    for (input_, target) in zip(X_valid, Y_valid):
+        
+        # format input and prediction
+        input_ = input_[np.newaxis,:]
+        target = np.array([target])[np.newaxis,:]
+        
+        net.activation(input_, accumulate=True)
+        
+        errors_valid[i] = net.output - target
+        
+        i += 1
+    
+    mean_valid, std_valid = (errors_valid.mean(), errors_valid.std())
+    
+    # test   
+    p_anomaly_test = np.zeros(shape=len(X_test))
+    predictions = np.zeros(shape=len(X_test))
+    errors_test = np.zeros(shape=len(Y_test))
+    threshold = utils.gaussian_pdf(mean_valid-2*std_valid, mean_valid, std_valid)
+    i = 0
+     
+    for (input_, target) in zip(X_test, Y_test):
+                
+        # format input and prediction
+        input_ = input_[np.newaxis,:]
+        target = np.array([target])[np.newaxis,:]
+                
+        net.activation(input_, accumulate=True)
+        prediction = net.output
+        
+        predictions[i] = prediction
+        errors_test[i] = scistats.norm.ppf(prediction-target, mean_valid, std_valid)
+        anomalies = np.argwhere(errors_test < threshold)   
+                
+        i += 1
+        
+    # plot results
+    fig, ax1 = plt.subplots()
+
+    # plot data series
+    ax1.plot(Y_test[:len(Y_test)-net.n_inputs+1], 'b', label='index')
+    ax1.set_xlabel('Date')
+    ax1.set_ylabel('TOPIX')
+
+    # plot predictions
+    ax1.plot(predictions, 'r', label='prediction')
+    ax1.set_ylabel('Change Point')
+    plt.legend(loc='best')
+    
+    for i in anomalies:
+        
+        if i <= len(Y_test)-net.n_inputs:
+            
+            plt.axvspan(i, i+1, color='yellow', alpha=0.5, lw=0)
+
+    fig.tight_layout()
+    plt.show()
+    
+    import random; print("\nTen couples of (prediction, target):\n",
+                         random.sample(set(zip(predictions, Y_test)), 10))    
+    
+    print("\nTotal error on ", len(predictions), "points is ", 
+          np.linalg.norm(Y_test[:len(Y_test)-net.n_inputs+1]-predictions))
+    
